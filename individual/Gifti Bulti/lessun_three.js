180a6449f24365c5d5abe9cbb96186dd49826ba0
giftiy <giftbulti59@gmail.com>
2024-11-26 13:31:47 +0300

    text

---
+++ individual/Gifti Bulti/lessun_three.js
@@ -0,0 +1,13 @@
+//2) Change the numbers inside the block, then try running the program again.
+//3) Try to place the rectangle near the bottom right of the screen.
+rect(10,200);
+rect(200, 200);
+// 1) Place two rectangles exactly in the corners of the screen
+rect(350,350);
+rect(0,0);
+// 2) Change the color from blue to yellow.
+fill("yellow");
+rect(100,100);
+rect(250, 100);
+rect(100, 200);
+// 3) Add a new square to the image
